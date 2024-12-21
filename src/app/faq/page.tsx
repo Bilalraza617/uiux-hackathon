22c652665545e08ed1759dafaed3f86e99c8a28a
Bilal raza <anwarbilal617@gmail.com>
2024-12-22 01:54:03 +0500

    ok

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,95 +1,96 @@
+import Brands from "@/components/Brands";
+import ShopHero from "@/components/ShopHero";
 import React from "react";
 
 const Faq = () => (
-  <div className="container mx-auto p-4 min-h-screen bg-gray-100">
-    <div className="bg-white p-6 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-6">
-      {/* General Information Section */}
-      <div>
-        <h1 className="text-3xl font-bold mb-4">General Information</h1>
-        <div className="space-y-4">
-          <div>
-            <h2 className="text-xl font-semibold">
-              Es dolorem eum ex sed enimend consectetur?
-            </h2>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
-              sed tristique nulla vitae, consequat gravida sagittis.
-            </p>
-          </div>
-          <div>
-            <h2 className="text-xl font-semibold">
-              Magna tincidunt est fermentum eros.
-            </h2>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
-              sed tristique nulla vitae, consequat gravida sagittis.
-            </p>
-          </div>
-          <div>
-            <h2 className="text-xl font-semibold">
-              Odio malesuada hac erat consequat sollicit?
-            </h2>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
-              sed tristique nulla vitae, consequat gravida sagittis.
-            </p>
-          </div>
-          <div>
-            <h2 className="text-xl font-semibold">
-              Eti id blandit aenean hac id velit quis nunc?
-            </h2>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
-              sed tristique nulla vitae, consequat gravida sagittis.
-            </p>
+  <>
+    <ShopHero title="FAQ" />
+    <div className=" mx-auto pt-20 w-[95%] text-[#1D3178]">
+      <div className=" flex  ">
+        {/* General Information Section */}
+        <div className=" w-[50%]">
+          <h1 className="text-3xl font-bold mb-10">General Information</h1>
+          <div className="">
+            <div className="mb-10">
+              <h2 className="text-xl font-extrabold mb-5">
+                Es dolorem eum ex sed enimend consectetur?
+              </h2>
+              <p className="text-[#A1ABCC]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Tristique sed <br /> tristique nulla vitae, consequat gravida
+                sagittis.
+              </p>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-xl font-extrabold mb-5">
+                Magna tincidunt est fermentum eros.
+              </h2>
+              <p className="text-[#A1ABCC]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Tristique sed <br />
+                tristique nulla vitae, consequat gravida sagittis.
+              </p>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-xl font-extrabold mb-5">
+                Odio malesuada hac erat consequat sollicit?
+              </h2>
+              <p className="text-[#A1ABCC]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Tristique sed <br />
+                tristique nulla vitae, consequat gravida sagittis.
+              </p>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-xl font-extrabold mb-5">
+                Eti id blandit aenean hac id velit quis nunc?
+              </h2>
+              <p className="text-[#A1ABCC]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Tristique sed <br />
+                tristique nulla vitae, consequat gravida sagittis.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Ask a Question Section */}
-      <div>
-        <h1 className="text-3xl font-bold mb-4">Ask a Question</h1>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700">
-              Your Name*
-            </label>
+        {/* Ask a Question Section */}
+        <div className="w-[50%] bg-[#F8F8FD]   px-5 py-14">
+          <h1 className="text-3xl font-bold  mb-20">Ask a Question</h1>
+          <form>
             <input
               type="text"
               id="name"
-              className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none"
+              placeholder="Your Name*"
+              className="w-full p-2 border border-gray-300 rounded mb-10 focus:outline-none"
             />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="subject" className="block text-gray-700">
-              Subject*
-            </label>
             <input
               type="text"
               id="subject"
-              className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none"
+              placeholder="Subject*"
+              className="w-full p-2 border border-gray-300 rounded mb-10 focus:outline-none"
             />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-gray-700">
-              Type Your Message*
-            </label>
+
             <textarea
+              placeholder="Type Your Message*"
               id="message"
-              className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none"
-              rows={4}
+              className="w-full p-2 border border-gray-300 rounded mb-10 focus:outline-none"
+              rows={7}
             ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-pink-600 text-white py-2 px-4 rounded-md hover:bg-pink-700"
-          >
-            Send Mail
-          </button>
-        </form>
+
+            <button
+              type="submit"
+              className=" bg-[#FB2E86] text-white py-2 px-8 rounded-md "
+            >
+              Send Mail
+            </button>
+          </form>
+          <div></div>
+        </div>
       </div>
     </div>
-  </div>
+    <Brands />
+  </>
 );
 
 export default Faq;
